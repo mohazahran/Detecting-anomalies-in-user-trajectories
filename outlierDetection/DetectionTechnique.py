Fixing the prob underflow
Using log probabilities

outlierDetection/DetectionTechnique.py
@@ -22,6 +22,8 @@ import sys
 from astropy import log
 sys.path.append('/Users/mohame11/anaconda/lib/python2.7/site-packages/')
 import kenlm
+import rnnlm
+import subprocess
 
 #testDic, quota, coreId, q, store, true_mem_size, hyper2id, obj2id, Theta_zh, Psi_sz, smoothedProbs
 class DetectionTechnique():
@@ -53,7 +55,8 @@ class DetectionTechnique():
     
     def getUserId(self, uid):
         pass
-        
+
+###################################################################################       
 class NgramLM (DetectionTechnique):
     def __init__(self):
         DetectionTechnique.__init__(self)  
@@ -68,7 +71,8 @@ class NgramLM (DetectionTechnique):
         #prob = self.model.score(strSeq, bos = True, eos = True)
         logProb = self.model.score(strSeq)
         #As is the custom in language modeling, all probabilities are log base 10.
-        return (10**logProb)
+        return logProb
+        #return (10**logProb)
         
     def prepareTestSet(self):
         testDic = {}
@@ -128,7 +132,107 @@ class NgramLM (DetectionTechnique):
     def getUserId(self, uid):
         return uid
         
+###################################################################################   
+class RNNLM (DetectionTechnique):
+    def __init__(self):
+        DetectionTechnique.__init__(self)  
+        self.type = SEQ_PROB.RNNLM
+        self.allActions = []
+        self.model = None
+       
+        
+    def loadModel(self):
+        '''
+        self.model = rnnlm.CRnnLM()
+        lmda = 0.75
+        regularization = 0.0000001
+        dynamic = 0
+        rand_seed = 1
+        self.model.setLambda(lmda)
+        self.model.setRegularization(regularization)
+        self.model.setDynamic(dynamic)
+        self.model.setRnnLMFile(self.model_path)
+        self.model.setRandSeed(rand_seed)
+        '''
+
+        
+    def getProbability(self, userId, newSeq, coreId):
+        #test_file = self.RESULTS_PATH+'tmp'+str(coreId)
+        test_file = '/Users/mohame11/Desktop/'+'tmp'+str(coreId)
+        w = open(test_file,'w')
+        strSeq = ' '.join(newSeq)
+        w.write(strSeq)
+        w.close()
+        
+        #self.model.setTestFile(test_file)
+        #prob = self.model.testNet() 
+        py2output = subprocess.check_output(['python', self.RNNLM_PYTHON_PATH+'main.py', '-rnnlm', self.model_path, '-test', test_file])
+        logProb = py2output.split('log probability:')[-1].split('PPL')[0].strip()
+        return logProb
+        #prob = 10**float(logProb)
+        #return prob
+        
+        
+        
+        
+    def prepareTestSet(self):
+        testDic = {}
+        print(">>> Preparing testset ...")
+        testSetCount = 0
+        r = open(self.SEQ_FILE_PATH, 'r')  
+        user = -1  
+        for line in r:
+            line = line.strip() 
+            tmp = line.split()  
+            actionStartIndex = 0
+            user += 1
+            if (self.DATA_HAS_USER_INFO == True):
+                user = tmp[0]   
+                actionStartIndex = 1
+            
+            if(self.VARIABLE_SIZED_DATA == True):
+                seq = tmp[actionStartIndex:]
+                goldMarkers = ['false']*len(seq)
+            else:
+                seq = tmp[actionStartIndex:self.true_mem_size+2]
+                goldMarkers = tmp[self.true_mem_size+2:]
+                if(len(goldMarkers) != len(seq)):
+                    goldMarkers = ['false']*len(seq)
+            for act in seq:
+                if(act not in self.allActions):
+                    self.allActions.append(act)
+            t = TestSample()  
+            t.user = user
+            t.actions = list(seq)
+            t.goldMarkers = list(goldMarkers)   
+            
+            testSetCount += 1
+            if(user in testDic):
+                testDic[user].append(t)                                                    
+            else:
+                testDic[user]=[t]
+        r.close()
+        if(self.useWindow == USE_WINDOW.FALSE): # we need to use the original sequence instead of overlapping windows
+            testSetCount = len(testDic)
+            for u in testDic:
+                tests = testDic[u]
+                originalSeq, originalGoldMarkers = self.formOriginalSeq(tests)
+                t = TestSample()  
+                t.user = u
+                t.actions = list(originalSeq)
+                t.goldMarkers = list(originalGoldMarkers)   
+                testDic[u] = [t]
+        return testDic, testSetCount    
+        
+              
+#self, testDic, quota, coreId, q, store, true_mem_size, hyper2id, obj2id, Theta_zh, Psi_sz, smoothedProbs
+
+    def getAllPossibleActions(self):
+        return self.allActions
     
+    def getUserId(self, uid):
+        return uid
+###################################################################################      
 class TribeFlow (DetectionTechnique):
     def __init__(self):
         DetectionTechnique.__init__(self)
@@ -215,7 +319,7 @@ class TribeFlow (DetectionTechnique):
             r = open(self.STAT_FILE, 'r')
             for line in r:
                 parts = line.strip().split('\t')                
-                self.smoothedProbs[parts[0]] = float(parts[1])                    
+                self.smoothedProbs[parts[0]] = math.log10(float(parts[1]))                    
             
         
         freqs = {}            
@@ -231,7 +335,7 @@ class TribeFlow (DetectionTechnique):
                 counts += 1
         for k in freqs:
             prob = float(freqs[k]+ smoothingParam) / float(counts + (len(freqs) * smoothingParam))
-            self.smoothedProbs[k] = prob
+            self.smoothedProbs[k] = math.log10(prob)
         
         w = open(self.STAT_FILE, 'w')
         for key in self.smoothedProbs:
@@ -244,13 +348,18 @@ class TribeFlow (DetectionTechnique):
         newSeqIds_np = np.array(newSeqIds, dtype = 'i4').copy()
         seqScore = _eval_outlier.calculateSequenceProb(newSeqIds_np, len(newSeqIds_np), self.true_mem_size, userId, self.Theta_zh, self.Psi_sz)                                            
         #seqScore = calculateSequenceProb(newSeq, true_mem_size, userId, obj2id, Theta_zh, Psi_sz)
+        logSeqScore = math.log10(seqScore)
         if(self.UNBIAS_CATS_WITH_FREQ):
-            unbiasingProb = 1.0
+            #unbiasingProb = 1.0
+            logUnbiasingProb = 0 
             for ac in newSeq:
                 if(ac in self.smoothedProbs):
-                    unbiasingProb *= self.smoothedProbs[ac]                                         
-            seqScore = float(seqScore)/float(unbiasingProb)  
-        return seqScore      
+                    #unbiasingProb *= self.smoothedProbs[ac]
+                    logUnbiasingProb += self.smoothedProbs[ac]                                          
+            #seqScore = float(seqScore)/float(unbiasingProb)  
+            logSeqScore = logSeqScore - logUnbiasingProb
+        #return seqScore
+        return logSeqScore      
     
     def prepareTestSet(self):
         testDic = {}