58aa41924b2db8c9327a83602f6ca92c1f223ed5
Mohamed A. Zahran <moh.a.zahran@gmail.com>
2017-02-04 17:28:00 -0500

    Using log probabilities

--- outlierDetection/_eval_outlier.pyx
+++ outlierDetection/_eval_outlier.pyx
@@ -1,4 +1,17 @@
 from cython import boundscheck, wraparound
+cdef extern from "<math.h>" nogil:
+    double log10(double x)
+
+
+
+@boundscheck(False)
+cpdef double getLogProb(double[:] logProbs, int listLen) nogil:
+    cdef double pw = 0.0
+    if(listLen == 1):
+        return logProbs[0]
+    
+    pw = (-1)*logProbs[0] + getLogProb(logProbs[1:], (listLen-1))
+    return logProbs[0]+log10(1+(10**pw))
 
 @boundscheck(False)
 cpdef double evaluate(int userId, int[:] history, int historyLen, int targetObjId, double[:, ::1] Theta_zh, double[:, ::1] Psi_sz, int env) nogil:        
@@ -10,10 +23,10 @@ cpdef double evaluate(int userId, int[:] history, int historyLen, int targetObjI
         mem_factor *= Psi_sz[history[j], env] # Psi[objId, env z]            
     #mem_factor *= 1.0 / (1 - Psi_sz[history[len(history)-1], env])# 1-Psi_sz[mem[B-1],z] == 1-psi_sz[objIdB,z]       
     candidateProb += mem_factor * Psi_sz[targetObjId, env] * Theta_zh[env, userId]                                              
-    return candidateProb
+    return log10(candidateProb)
 
 @boundscheck(False)
-cpdef double calculateSequenceProb(int[:] theSequence, int theSequenceLen, int true_mem_size, int userId, double[:, ::1] Theta_zh, double[:, ::1] Psi_sz) nogil:                     
+cpdef double calculateSequenceProb(int[:] theSequence, int theSequenceLen, double[:] logSeqProbZ, int true_mem_size, int userId, double[:, ::1] Theta_zh, double[:, ::1] Psi_sz) nogil:                     
     cdef double seqProb = 0.0   
     cdef double seqProbZ = 1.0    
     cdef int targetObjId = -1
@@ -25,21 +38,22 @@ cpdef double calculateSequenceProb(int[:] theSequence, int theSequenceLen, int t
     cdef int historyLen = 0
     cdef int targetObjIdx, z, i = 0
     cdef int wmax = 0
-    
+   
     window = min(true_mem_size, theSequenceLen)
     for z in xrange(Psi_sz.shape[1]): #for envs
-        seqProbZ = 1.0        
+        seqProbZ = 0.0        
         for targetObjIdx in range(0,theSequenceLen): #targetObjIdx=0 cannot be predicted we have to skip it
             if(targetObjIdx == 0):                
                 targetObjId = theSequence[targetObjIdx]                            
                 prior = Psi_sz[targetObjId, z]
-                seqProbZ *= prior
+                seqProbZ += log10(prior)
             else:                                                                            
                 targetObjId = theSequence[targetObjIdx]      
                 wmax = max(0,targetObjIdx-window)           
                 history = theSequence[wmax: targetObjIdx] # look back 'window' actions.                                                            
                 historyLen = targetObjIdx-wmax                            
                 candProb = evaluate(userId, history, historyLen, targetObjId, Theta_zh, Psi_sz, z) #(int[:, ::1] HOs, double[:, ::1] Theta_zh, double[:, ::1] Psi_sz, int[::1] count_z, int env):                                
-                seqProbZ *= candProb              
-        seqProb += seqProbZ       
-    return seqProb   
+                seqProbZ += candProb
+        logSeqProbZ[z] = seqProbZ             
+        #seqProb += seqProbZ     
+    return getLogProb(logSeqProbZ, Psi_sz.shape[1])    
